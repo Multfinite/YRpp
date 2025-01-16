c4f48ec0475f0053e48cf8c14b35a04f28e9b45c
Multfinite <multfinite@gmail.com>
2025-01-16 20:11:37 +0300

    AbstractTypeClass revision.

--- AbstractTypeClass.h
+++ AbstractTypeClass.h
@@ -1,10 +1,7 @@
-/*
-	AbstractTypes are abstract template objects initialized by INI files
-*/
 #pragma once
 
-#include <AbstractClass.h>
-#include <Memory.h>
+#include "AbstractClass.h"
+#include "Memory.h"
 
 //forward declarations
 class CCINIClass;
@@ -26,47 +23,42 @@ class CCINIClass;
 				return i;\
 		return -1;\
 	}
-//---
 
+/*!
+* @brief AbstractType is abstract template objects initialized by INI files.
+*/
 class NOVTABLE AbstractTypeClass : public AbstractClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::Abstract;
+	static constexpr uintptr_t AbsVTable = 0x7E2000;
+	static constexpr AbstractType AbsID = AbstractType::Abstract;
 
-	//Static
 	static constexpr constant_ptr<DynamicVectorClass<AbstractTypeClass*>, 0xA8E968u> const Array{};
+public:
+	char ID[0x19-1];
+	/*! @brief Nullterminator of ID (some cursed string handler class do this) */
+	PROTECTED_PROPERTY(BYTE, __ID_ZERO);
+	char UINameLabel[0x20-1];
+	/*! @brief Nullterminator of ID (some cursed string handler class do this) */
+	PROTECTED_PROPERTY(BYTE, __UINameLabel_ZERO);
+	const wchar_t* UIName;
+	char Name[0x31];
+public:
+	void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x410BE0);
 
-	//Destructor
-	virtual ~AbstractTypeClass() RX;
+	virtual ~AbstractTypeClass() JMP_THIS(0x4109C0);
+	/*!
+	* @brief It's commonly empty. Only AnimClass override this.
+	* @note vtable_index 24:0x60
+	*/
+	virtual void LoadTheaterSpecificArt(TheaterType th_type) JMP_THIS(0x410C20);
+	/*25:0x64*/virtual bool LoadFromINI(CCINIClass* pINI) JMP_THIS(0x410A60);
+	/*26:0x68*/virtual bool SaveToINI(CCINIClass* pINI) JMP_THIS(0x410B90);
 
-	//AbstractTypeClass
-	virtual void LoadTheaterSpecificArt(TheaterType th_type) RX;
-	virtual bool LoadFromINI(CCINIClass* pINI) R0;
-	virtual bool SaveToINI(CCINIClass* pINI) R0;
-
-	const char* get_ID() const {
-		return this->ID;
-	}
-
-	//Constructor
-	AbstractTypeClass(const char* pID) noexcept
-		: AbstractTypeClass(noinit_t())
-	{ JMP_THIS(0x410800); }
+	const char* get_ID() const { return this->ID; }
 
 protected:
-	explicit __forceinline AbstractTypeClass(noinit_t) noexcept
-		: AbstractClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
-
-public:
-
-	char ID [0x18];
-	PROTECTED_PROPERTY(BYTE, zero_3C);
-	char UINameLabel [0x20];
-	const wchar_t* UIName;
-	char Name [0x31];
+	explicit __forceinline AbstractTypeClass(fake_noinit_t) noexcept{ }
+	AbstractTypeClass(noinit_t) : AbstractTypeClass(fake_noinit_t{}) JMP_THIS(0x410960);
+	AbstractTypeClass(char* pId) : AbstractTypeClass(fake_noinit_t{}) JMP_THIS(0x410800);
 };
