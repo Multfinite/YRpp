692212abdcc497055701b7792e8fa7de85ab17b9
Multfinite <multfinite@gmail.com>
2025-03-23 13:45:37 +0300

    InfantryClass revision #1

--- InfantryClass.h
+++ InfantryClass.h
@@ -1,26 +1,42 @@
-/*
-	Infantry
-*/
-
 #pragma once
 
-#include <FootClass.h>
-#include <InfantryTypeClass.h>
+#include "FootClass.h"
+#include "InfantryTypeClass.h"
 
+/*!
+* @brief Infantry
+*/
 class NOVTABLE InfantryClass : public FootClass
 {
 public:
 	static const AbstractType AbsID = AbstractType::Infantry;
 	static constexpr uintptr_t AbsVTable = 0x7EB058;
+	static constexpr size_t ClassSize = 0x6F0;
 
-	//Static
 	static constexpr constant_ptr<DynamicVectorClass<InfantryClass*>, 0xA83DE8u> const Array{};
-
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
+public:
+	InfantryTypeClass* Type;
+	//which is currently playing
+	Sequence SequenceAnim;
+	CDTimerClass CommentTimer;
+	// set in ReceiveDamage on panicky units
+	DWORD          PanicDurationLeft;
+	// set by script action, not cleared anywhere
+	bool           PermanentBerzerk;
+	bool           IsTechnician;
+	bool           IsStroked;
+	bool           IsProne;
+	bool           IsZoneCheat;
+	bool           WasSelected;
+	DWORD      unknown_6E0;
+	bool           ShouldDeploy;
+	int            OnLand;
+	PROTECTED_PROPERTY(DWORD, unused_6EC);
+public:
+	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) override JMP_THIS(0x523300);
 
 	//Destructor
-	virtual ~InfantryClass() RX;
+	virtual ~InfantryClass() JMP_THIS(0x517D90);
 
 	//AbstractClass
 	virtual RTTIType KindOf() const RT(AbstractType);
@@ -47,25 +63,4 @@ protected:
 	explicit __forceinline InfantryClass(noinit_t) noexcept
 		: FootClass(noinit_t())
 	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
-
-public:
-
-	InfantryTypeClass* Type;
-	Sequence SequenceAnim; //which is currently playing
-	CDTimerClass unknown_Timer_6C8;
-	DWORD          PanicDurationLeft; // set in ReceiveDamage on panicky units
-	bool           PermanentBerzerk; // set by script action, not cleared anywhere
-	bool           Technician;
-	bool           unknown_bool_6DA;
-	bool           Crawling;
-	bool           unknown_bool_6DC;
-	bool           unknown_bool_6DD;
-	DWORD          unknown_6E0;
-	bool           ShouldDeploy;
-	int            unknown_int_6E8;
-	PROTECTED_PROPERTY(DWORD, unused_6EC); //??
 };
