7ea69e947ba173402e97be3d5e6a1fb39c13a3ef
Multfinite <multfinite@gmail.com>
2025-03-30 10:03:50 +0300

    BuildingClass revision #1

--- BuildingClass.h
+++ BuildingClass.h
@@ -1,13 +1,9 @@
-/*
-	Buildings
-*/
-
 #pragma once
 
-#include <TechnoClass.h>
-#include <BuildingTypeClass.h>
-#include <BuildingLightClass.h>
-#include <StageClass.h>
+#include "TechnoClass.h"
+#include "BuildingTypeClass.h"
+#include "BuildingLightClass.h"
+#include "StageClass.h"
 
 class FactoryClass;
 class InfantryClass;
@@ -26,15 +22,168 @@ enum class BStateType : unsigned int
 	None = 0xFFFFFFFF,
 };
 
+enum class BAnimType : unsigned int
+{
+	Uograde_1 = 0x0,
+	Upgrade_2 = 0x1,
+	Upgrade_3 = 0x2,
+	Active_1 = 0x3,
+	Active_2 = 0x4,
+	Active_3 = 0x5,
+	Active_4 = 0x6,
+	PreProduction = 0x7,
+	Production = 0x8,
+	Turret = 0x9,
+	Special_1 = 0xA,
+	Special_2 = 0xB,
+	Special_3 = 0xC,
+	Special_4 = 0xD,
+	Super_1 = 0xE,
+	Super_2 = 0xF,
+	Super_3 = 0x10,
+	Super_4 = 0x11,
+	Idale = 0x12,
+	LowPower = 0x13,
+	SuperLowPower = 0x14,
+	Count = 0x15,
+	M2 = 0xFFFFFFFE // ???
+};
+
+
+/*!
+* @brief Buildings
+*/
 class NOVTABLE BuildingClass : public TechnoClass
 {
 public:
+	using base_type = TechnoClass;
+	struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+	{
+		constexpr vtables_t() noexcept : base_type::vtables_t()
+		{
+			this->IPersistStream = 0x7E3EBC;
+			this->IRTTITypeInfo = 0x7E3EA0;
+			this->INoticeSink = 0x7E3E98;
+			this->INoticeSource = 0x7E3E90;
+		}
+	};
+	static inline vtables_t vtables{};
+public:
 	static const AbstractType AbsID = AbstractType::Building;
 	static constexpr uintptr_t AbsVTable = 0x7E3EBC;
+	static constexpr size_t ClassSize = 0x720;
 
-	//Static
 	static constexpr constant_ptr<DynamicVectorClass<BuildingClass*>, 0xA8EB40u> const Array{};
+public:
+	BuildingTypeClass* Type;
+	FactoryClass* Factory;
+	CDTimerClass C4Timer;
+	BStateType BState;
+	BStateType QueueBState;
+	DWORD OwnerCountryIndex;
+	InfantryClass* C4AppliedBy;
+	DWORD LastStrength;
+	AnimClass* FirestormAnim;
+	AnimClass* PsiWarnAnim;
+	CDTimerClass FactoryRetryTimer;
 
+	// see eBuildingAnims above for slot index meanings
+	AnimClass* Anims[static_cast<size_t>(BAnimType::Count)];
+	// one flag for each of the above anims (whether the anim was enabled when power went offline?)
+	bool AnimStates[static_cast<size_t>(BAnimType::Count)];
+
+protected:
+	char align_5C5[3];
+public:
+
+	AnimClass* DamageFireAnims[0x8];
+	// if set, ::Update spawns damage fire anims and zeroes it
+	bool RequiresDamageFires; 
+	//5E8 - 5F8 ????????
+	BuildingTypeClass* Upgrades[0x3];
+
+	// type # of sw being launched
+	int FiringSWType;
+	DWORD TurretAnimLetterIndex;
+	BuildingLightClass* Spotlight;
+	RateTimer GateTimer;
+	// tiled light , LightIntensity > 0
+	LightSourceClass* LightSource;
+	// 0-7 for active directionals, 8/12 for offline ones, check ntfnce.shp or whatever
+	DWORD LaserFenceFrame;
+	// anim data for firestorm active animations
+	DWORD FirestormWallFrame;
+	// for hospital, armory, unitrepair etc
+	StageClass RepairProgress;
+	RectangleStruct unknown_rect_63C;
+	CoordStruct unknown_coord_64C;
+	Point2D unknown_point_658;
+	DWORD unknown_65C;
+	bool HasPower;
+	bool IsOverpowered;
+	// each powered unit controller building gets this set on power activation and unset on power outage
+	bool RegisteredAsPoweredUnitSource;
+	DWORD SupportingPrisms;
+	bool HasExtraPowerBonus;
+	bool HasExtraPowerDrain;
+	DynamicVectorClass<InfantryClass*> Overpowerers;
+	DynamicVectorClass<InfantryClass*> Occupants;
+	// which occupant should get XP, which weapon should be fired (see 6FF074)
+	int FiringOccupantIndex;
+	AudioController ConstructAudio;
+	AudioController BuildingAudio;
+	// the the last state when Update()ing. if this changed since the last Update(), UpdatePowered is called.
+	bool WasOnline;
+	// is also NOMINAL under [Structures]
+	bool ShowRealName;
+	// is also AI_REBUILDABLE under [Structures]
+	bool BeingProduced;
+	// is also AI_REPAIRABLE under [Structures]
+	bool ShouldRebuild;
+	// used to pass the NeedsEngineer check
+	bool HasEngineer;
+	CDTimerClass CashProductionTimer;
+	// AI_SELLABLE under [Structures]
+	bool IsAIAllowedToSell;
+	bool IsReadyToCommence;
+	// AI handholder for repair logic,
+	bool NeedsRepairs;
+	// C4Applied
+	bool IsGoingToBlow;
+	bool NoCrew;
+	bool IsCharging;
+	bool IsCharged;
+	// has this building changed ownership at least once? affects crew and repair.
+	bool HasBeenCaptured;
+	bool ActuallyPlacedOnMap;
+	bool unknown_bool_6E5;
+	// AI handholder for repair logic,
+	bool IsDamaged;
+	bool IsFogged;
+	bool IsBeingRepaired; // show animooted repair wrench
+	bool HasBuildUp;
+	// status set by EnableStuff() and DisableStuff()
+	bool StuffEnabled;
+	// some fugly buffers
+	char HasCloakingData;
+	// from Type->CloakRadiusInCells
+	byte CloakRadius; 
+	char Translucency;
+	// the old "silo needed" logic
+	DWORD StorageFilledSlots;
+	// randomly assigned secret lab bonus, used if SecretInfantry, SecretUnit, and SecretBuilding are null
+	TechnoTypeClass* SecretProduction;
+	ColorStruct ColorAdd;
+	int unknown_int_6FC;
+	short unknown_short_700;
+	// as defined by Type->UpgradesToLevel=
+	BYTE UpgradeLevel;
+	char GateStage;
+	PrismChargeState PrismStage;
+	Coordinate PrismTargetCoords;
+	DWORD DelayBeforeFiring;
+	int BunkerState; // used in UpdateBunker and friends
+public:
 	//IPersist
 	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
 
@@ -250,101 +399,4 @@ protected:
 	explicit __forceinline BuildingClass(noinit_t) noexcept
 		: TechnoClass(noinit_t())
 	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
-
-public:
-
-	BuildingTypeClass* Type;
-	FactoryClass* Factory;
-	CDTimerClass C4Timer;
-	int BState;
-	int QueueBState;
-	DWORD OwnerCountryIndex;
-	InfantryClass* C4AppliedBy;
-	DWORD unknown_544;
-	AnimClass* FirestormAnim; //pointer
-	AnimClass* PsiWarnAnim; //pointer
-	CDTimerClass FactoryRetryTimer;
-
-// see eBuildingAnims above for slot index meanings
-	AnimClass * Anims [0x15];
-	bool AnimStates [0x15]; // one flag for each of the above anims (whether the anim was enabled when power went offline?)
-
-protected:
-	char align_5C5[3];
-public:
-
-	AnimClass * DamageFireAnims [0x8];
-
-	bool RequiresDamageFires; // if set, ::Update spawns damage fire anims and zeroes it
-	//5E8 - 5F8 ????????
-	BuildingTypeClass * Upgrades [0x3];
-
-	int FiringSWType; // type # of sw being launched
-	DWORD unknown_5FC;
-	BuildingLightClass* Spotlight;
-	RateTimer GateTimer;
-	LightSourceClass * LightSource; // tiled light , LightIntensity > 0
-	DWORD LaserFenceFrame; // 0-7 for active directionals, 8/12 for offline ones, check ntfnce.shp or whatever
-	DWORD FirestormWallFrame; // anim data for firestorm active animations
-	StageClass RepairProgress; // for hospital, armory, unitrepair etc
-	RectangleStruct unknown_rect_63C;
-	CoordStruct unknown_coord_64C;
-	int unknown_int_658;
-	DWORD unknown_65C;
-	bool HasPower;
-	bool IsOverpowered;
-
-	// each powered unit controller building gets this set on power activation and unset on power outage
-	bool RegisteredAsPoweredUnitSource;
-
-	DWORD SupportingPrisms;
-	bool HasExtraPowerBonus;
-	bool HasExtraPowerDrain;
-	DynamicVectorClass<InfantryClass*> Overpowerers;
-	DynamicVectorClass<InfantryClass*> Occupants;
-	int FiringOccupantIndex; // which occupant should get XP, which weapon should be fired (see 6FF074)
-
-	AudioController Audio7;
-	AudioController Audio8;
-
-	bool WasOnline; // the the last state when Update()ing. if this changed since the last Update(), UpdatePowered is called.
-	bool ShowRealName; // is also NOMINAL under [Structures]
-	bool BeingProduced; // is also AI_REBUILDABLE under [Structures]
-	bool ShouldRebuild; // is also AI_REPAIRABLE under [Structures]
-	bool HasEngineer; // used to pass the NeedsEngineer check
-	CDTimerClass CashProductionTimer;
-	bool AI_Sellable; // AI_SELLABLE under [Structures]
-	bool IsReadyToCommence;
-	bool NeedsRepairs; // AI handholder for repair logic,
-	bool C4Applied;
-	bool NoCrew;
-	bool unknown_bool_6E1;
-	bool unknown_bool_6E2;
-	bool HasBeenCaptured; // has this building changed ownership at least once? affects crew and repair.
-	bool ActuallyPlacedOnMap;
-	bool unknown_bool_6E5;
-	bool IsDamaged; // AI handholder for repair logic,
-	bool IsFogged;
-	bool IsBeingRepaired; // show animooted repair wrench
-	bool HasBuildUp;
-	bool StuffEnabled; // status set by EnableStuff() and DisableStuff()
-	char HasCloakingData; // some fugly buffers
-	byte CloakRadius; // from Type->CloakRadiusInCells
-	char Translucency;
-	DWORD StorageFilledSlots; // the old "silo needed" logic
-	TechnoTypeClass * SecretProduction; // randomly assigned secret lab bonus, used if SecretInfantry, SecretUnit, and SecretBuilding are null
-	ColorStruct ColorAdd;
-	int unknown_int_6FC;
-	short unknown_short_700;
-	BYTE UpgradeLevel; // as defined by Type->UpgradesToLevel=
-	char GateStage;
-	PrismChargeState PrismStage;
-	CoordStruct PrismTargetCoords;
-	DWORD DelayBeforeFiring;
-
-	int BunkerState; // used in UpdateBunker and friends
 };
