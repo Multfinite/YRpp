8cb7a8c752a42704a4bfc6fee29f9ac44ab49470
Multfinite <multfinite@gmail.com>
2025-03-30 18:07:24 +0300

    ObjectTypeClass revision #3

--- ObjectTypeClass.h
+++ ObjectTypeClass.h
@@ -74,26 +74,116 @@ public:
 	IndexClass<ShadowVoxelIndexKey, VoxelCacheStruct*> VoxelShadowCache;
 	IndexClass<TurretBarrelVoxelIndexKey, VoxelCacheStruct*> VoxelTurretBarrelCache;
 public:
+	virtual ~ObjectTypeClass() JMP_THIS(0x5F7400);
+	
 	HRESULT __stdcall Load(IStream* pStm) override JMP_THIS(0x5F9720);
 	HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x5F9950);
 	HRESULT __stdcall GetSizeMax(ULARGE_INTEGER* pcbSize) override JMP_THIS(0x5F9970);
 
 	bool LoadFromINI(CCINIClass* pINI) override JMP_THIS(0x5F92D0);
 
-	virtual ~ObjectTypeClass() JMP_THIS(0x5F7400);
+	/*!
+	* @brief
+	* @note original_name Coord_Fixup
+	* @note vtable_index 27:0x6C
+	* @note address 0x41CF80
+	*/
 	virtual CoordStruct FixupCoord(CoordStruct& coord) const JMP_THIS(0x41CF80);
+
+	/*!
+	* @brief
+	* @note original_name Get_Ownable
+	* @note vtable_index 28:0x70
+	* @note address 0x428E40
+	*/
 	virtual DWORD Ownable() const JMP_THIS(0x428E40);
+
+	/*!
+	* @brief
+	* @note original_name Max_Pips
+	* @note vtable_index 29:0x74
+	* @note address 0x5F75B0
+	*/
 	virtual int MaxPips() const JMP_THIS(0x5F75B0);
+
+	/*!
+	* @brief
+	* @note original_name Pixel_Dimensions
+	* @note vtable_index 30:0x78
+	* @note address 0x5F75C0
+	*/
 	virtual Point3D PixelDimensions() const JMP_THIS(0x5F75C0);
+
+	/*!
+	* @brief
+	* @note original_name Lepton_Dimensions
+	* @note vtable_index 31:0x7C
+	* @note address 0x5F75E0
+	*/
 	virtual Point3D LeptonDimensions() const JMP_THIS(0x5F75E0);
+
+	/*!
+	* @brief
+	* @note original_name Create_And_Place
+	* @note vtable_index 32:0x80
+	* @note address 0x4C9150
+	*/
 	virtual bool InstantiateAt(CellStruct& position, HouseClass* pOwner) = 0;
+
+	/*!
+	* @brief
+	* @note original_name Cost_Of
+	* @note vtable_index 33:0x84
+	* @note address 0x5F7610
+	*/
 	virtual int CostOf(HouseClass* pHouse) const JMP_THIS(0x5F7610);
+
+	/*!
+	* @brief
+	* @note original_name Time_To_Build
+	* @note vtable_index 34:0x88
+	* @note address 0x5F7620
+	*/
 	virtual int TimeToBuild() const JMP_THIS(0x5F7620);
+
+	/*!
+	* @brief
+	* @note original_name Create_One_Of
+	* @note vtable_index 35:0x8C
+	* @note address 0x4C9150
+	*/
 	virtual ObjectClass* Instantiate(HouseClass* pOwner) = 0;
-	/*! @brief list of occypied cells. Foundation for buildings. placement was called includeBib */
+
+	/*!
+	* @brief list of occypied cells. Foundation for buildings. placement was called includeBib
+	* @note original_name Occupy_List
+	* @note vtable_index 36:0x90
+	* @note address 0x5F7640
+	*/
 	virtual CellStruct* OccupiedCells(bool placement) const JMP_THIS(0x5F7640);
+
+	/*!
+	* @brief
+	* @note original_name Who_Can_Build_Me
+	* @note vtable_index 37:0x94
+	* @note address 0x5F7900
+	*/
 	virtual BuildingClass* FindFactory(bool allowOccupied, bool requirePower, bool requireCanBuild, HouseClass const* pHouse) const JMP_THIS(0x5F7900);
+
+	/*!
+	* @brief
+	* @note original_name Get_Cameo_Data
+	* @note vtable_index 38:0x98
+	* @note address 0x5F7630
+	*/
 	virtual SHPStruct* GetCameo() const JMP_THIS(0x5F7630);
+
+	/*!
+	* @brief
+	* @note original_name Get_Image_Data
+	* @note vtable_index 39:0x9C
+	* @note address 0x41CFA0
+	*/
 	virtual SHPStruct* GetImage() const JMP_THIS(0x41CFA0);
 
 	/*! @brief Return BuildCat for Buildings of BuildingTypes, otherwise - false*/
@@ -122,6 +212,7 @@ protected:
 		, RadialColor{}, Armor{}, MainVoxel{}, TurretVoxel{}, BarrelVoxel{}, LineTrailColor{}
 		, VoxelMainCache{}, VoxelTurretWeaponCache{}, VoxelShadowCache{}, VoxelTurretBarrelCache{}
 	{ }
+
 	ObjectTypeClass(noinit_t) : ObjectTypeClass(fake_noinit_t{}) JMP_THIS(0x5F7320);
 	ObjectTypeClass(const char* pId) : ObjectTypeClass(fake_noinit_t{}) JMP_THIS(0x5F7090);
 };
