6570497610318ebc8f04feeaad10f5b8bf389e2f
Multfinite <multfinite@gmail.com>
2025-04-19 18:39:27 +0300

    AircraftTypeClass revision #2

--- AircraftTypeClass.h
+++ AircraftTypeClass.h
@@ -5,10 +5,22 @@
 class NOVTABLE AircraftTypeClass : public TechnoTypeClass
 {
 public:
+	using base_type = TechnoTypeClass;
+	struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+	{
+		constexpr vtables_t() noexcept : base_type::vtables_t()
+		{
+			this->IPersistStream = 0x7E2868;
+			this->IRTTITypeInfo = 0x7E284C;
+			this->INoticeSink = 0x7E2844;
+			this->INoticeSource = 0x7E283C;
+		}
+	};
+	static inline vtables_t vtables{};
+public:
 	static const AbstractType AbsID = AbstractType::AircraftType;
 	static constexpr uintptr_t AbsVTable = 0x7E2868;
 
-	//Array
 	ABSTRACTTYPE_ARRAY(AircraftTypeClass, 0xA8B218u);
 public:
 	int ArrayIndex;
@@ -23,11 +35,10 @@ public:
 	bool AirportBound;
 	bool Fighter;
 public:
+	virtual ~AircraftTypeClass() JMP_THIS(0x41CA20);
+
 	//IPersist
 	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//Destructor
-	virtual ~AircraftTypeClass() RX;
 
 	//AbstractClass
 	virtual RTTIType KindOf() const RT(AbstractType);
@@ -39,13 +50,9 @@ public:
 
 	//TechnoTypeClass
 
-	//Constructor
-	AircraftTypeClass(const char* pID) noexcept
-		: AircraftTypeClass(noinit_t())
-	{ JMP_THIS(0x41C8B0); }
-
 protected:
-	explicit __forceinline AircraftTypeClass(noinit_t) noexcept
-		: TechnoTypeClass(noinit_t())
-	{ }
+	explicit __forceinline AircraftTypeClass(fake_noinit_t) noexcept : TechnoTypeClass(fake_noinit_t{}) {}
+public:
+	AircraftTypeClass(noinit_t) : AircraftTypeClass(fake_noinit_t{}) JMP_THIS(0x41C9F0);
+	AircraftTypeClass(const char* pId) : AircraftTypeClass(fake_noinit_t{}) JMP_THIS(0x41C8B0);
 };
