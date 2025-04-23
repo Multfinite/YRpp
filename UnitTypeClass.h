2a896d66f9d3f25cec6f0cd6b0a2da22f1a6d179
Multfinite <multfinite@gmail.com>
2025-04-23 20:48:31 +0300

    UnitTypeClass revision #3

--- UnitTypeClass.h
+++ UnitTypeClass.h
@@ -65,35 +65,33 @@ public:
 	char FiringFrames;
 	char AltImageFile[0x19];
 public:
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
+	virtual ~UnitTypeClass() JMP_THIS(0x7472F0);
 
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~UnitTypeClass() RX;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	//AbstractTypeClass
-
-	//ObjectTypeClass
-	virtual bool InstantiateAt(CellStruct* pMapCoords, HouseClass* pOwner) R0;
-	virtual ObjectClass* Instantiate(HouseClass* pOwner) R0;
-
-	//TechnoTypeClass
-
-	//Constructor
-	UnitTypeClass(const char* pID) noexcept
-		: UnitTypeClass(noinit_t())
-	{ JMP_THIS(0x7470D0); }
+	HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x747F30);
+	
+	HRESULT Load(IStream* pStm) override JMP_THIS(0x748010);
+	HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x7480B0);
+	
+	RTTIType KindOf() const override JMP_THIS(0x748170);
+	int SizeOf() const override JMP_THIS(0x748160);
+	void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x747F70);
+	int ArrayIndex() const override JMP_THIS(0x748180);
+	
+	bool LoadFromINI(CCINIClass* pINI) override JMP_THIS(0x747620);
+	
+	CoordStruct FixupCoord(CoordStruct& coord) const override JMP_THIS(0x747EB0);
+	Point3D PixelDimensions() const override JMP_THIS(0x747590);
+	Point3D LeptonDimensions() const override JMP_THIS(0x7475D0);
+	bool InstantiateAt(CellStruct& position, HouseClass* pOwner) override JMP_THIS(0x7474B0);
+	ObjectClass* Instantiate(HouseClass* pOwner) override JMP_THIS(0x747560);
+	
+	bool CanUseWaypoint() const override JMP_THIS(0x7473C0);
+	bool CanAttackMove() const override JMP_THIS(0x7473D0);
+	int GetRepairStep() const override JMP_THIS(0x747F20);
 
 protected:
-	explicit __forceinline UnitTypeClass(noinit_t) noexcept
-		: TechnoTypeClass(noinit_t())
-	{ }
+	explicit __forceinline UnitTypeClass(fake_noinit_t) noexcept : TechnoTypeClass(fake_noinit_t{}) {}
+public:
+	UnitTypeClass(noinit_t) : UnitTypeClass(fake_noinit_t{}) JMP_THIS(0x7472C0);
+	UnitTypeClass(const char* pId) : UnitTypeClass(fake_noinit_t{}) JMP_THIS(0x7470D0);
 };
