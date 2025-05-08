20763d24852b0ab7304deeab6481a6a4253821c9
Multfinite <multfinite@gmail.com>
2025-05-08 12:57:05 +0300

    AircraftTrackerClass revision #1

--- AircraftTrackerClass.h
+++ AircraftTrackerClass.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <Helpers/CompileTime.h>
+#include "Helpers/CompileTime.h"
 
-#include <CellClass.h>
-#include <GeneralStructures.h>
-#include <ArrayClasses.h>
+#include "CellClass.h"
+#include "GeneralDefinitions.h"
+#include "ArrayClasses.h"
 
 class TechnoClass;
+class CellClass;
 
 // Tracks aerial units via 20x20 vectors spread across the maps for efficient search
 class AircraftTrackerClass
@@ -15,19 +16,19 @@ public:
 	static constexpr reference<AircraftTrackerClass, 0x887888u> Instance { };
 
 	// Fills CurrentVector with items from TrackerVectors matching given range around cell.
-	void FillCurrentVector(CellClass* pCell, int range) { JMP_THIS(0x412B40) }
+	void FillCurrentVector(CellClass* pCell, int range) JMP_THIS(0x412B40)
 
 	// Gets items from CurrentVector.
-	TechnoClass* Get() { JMP_THIS(0x4137A0) }
+	TechnoClass* Get() JMP_THIS(0x4137A0)
 
-	void Add(TechnoClass* entry) { JMP_THIS(0x4134A0) }
-	void AI(TechnoClass* entry, CellStruct oldPos, CellStruct newPos) { JMP_THIS(0x4138C0) }
-	void Remove(TechnoClass* entry) { JMP_THIS(0x4135D0) }
+	void Add(TechnoClass* entry) JMP_THIS(0x4134A0)
+	void AI(TechnoClass* entry, CellStruct oldPos, CellStruct newPos) JMP_THIS(0x4138C0)
+	void Remove(TechnoClass* entry) JMP_THIS(0x4135D0)
 
 	bool Clear() { JMP_THIS(0x413800) }
 
-	bool IsJumpjet(TechnoClass* entry) { JMP_THIS(0x4135A0) }
-	int GetVectorIndex(CellStruct pos) { JMP_THIS(0x412AC0) }
+	bool IsJumpjet(TechnoClass* entry) JMP_THIS(0x4135A0)
+	int GetVectorIndex(CellStruct pos) JMP_THIS(0x412AC0)
 
 	// TODO write other entries
 
