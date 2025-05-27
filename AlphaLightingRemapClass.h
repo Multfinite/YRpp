74207285aad4e01c661c2375b4f3c4569604a76d
Multfinite <multfinite@gmail.com>
2025-05-27 23:40:10 +0300

    AlphaLightingRemapClass revision #1

--- AlphaLightingRemapClass.h
+++ AlphaLightingRemapClass.h
@@ -1,35 +1,26 @@
 #pragma once
 
-#include <YRPP.h>
-#include <Helpers/CompileTime.h>
+#include "YRPP.h"
+#include "Helpers/CompileTime.h"
+#include "ArrayClasses.h"
 
 class AlphaLightingRemapClass
 {
 public:
 	constexpr static reference<DynamicVectorClass<AlphaLightingRemapClass*>, 0x88A080> const Array{};
-
+public:
+	uint16_t Table[256][256];
+	int IntensityCount;
+	int RefCount;
+public:
 	// Notice:
 	// When a ConvertClass is constructed by the game, it will generate [IntensityCount] color
 	// tables from dark to bright. Each of them just changes the intensity of the source palette.
 	//
 	// If we have a point, whose value in ABuffer is A
-	static AlphaLightingRemapClass* __stdcall FindOrAllocate(int intensityCount)
-	{
-		JMP_STD(0x420140);
-	}
-
-	static void __stdcall Release(AlphaLightingRemapClass* pItem)
-	{
-		JMP_STD(0x420270);
-	}
+	constexpr static AlphaLightingRemapClass* __stdcall FindOrAllocate(int intensityCount) JMP_STD(0x420140);
+	constexpr static void __stdcall Release(AlphaLightingRemapClass* pItem) JMP_STD(0x420270);
 
 	// The game never directly called this function, it just call FindOrAllocate and this CTOR is inlined!
-	explicit AlphaLightingRemapClass(int intensityCount) noexcept
-	{
-		JMP_THIS(0x4202F0);
-	}
-
-	WORD Table[256][256];
-	int IntensityCount;
-	int RefCount;
+	explicit AlphaLightingRemapClass(int intensityCount) noexcept JMP_THIS(0x4202F0);
 };
