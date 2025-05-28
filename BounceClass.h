f388bd1374cbae0965c901362fe50e85b34e8852
Multfinite <multfinite@gmail.com>
2025-05-28 23:25:02 +0300

    BounceClass revision #1

--- BounceClass.h
+++ BounceClass.h
@@ -1,12 +1,11 @@
-/*
-	Ballistic trajectory data used by Animations and Voxel Animations.
-*/
-
 #pragma once
 
-#include <GeneralStructures.h>
-#include <YRMathVector.h>
+#include "GeneralStructures.h"
+#include "YRMathVector.h"
 
+/*!
+* Ballistic trajectory data used by Animations and Voxel Animations.
+*/
 class BounceClass
 {
 public:
@@ -15,41 +14,7 @@ public:
 		Bounce = 1,
 		Impact = 2
 	};
-
-	// constructors
-	BounceClass() = default;
-
-	BounceClass(const CoordStruct& coords, double elasticity, double gravity,
-		double maxVelocity, const Vector3D<float>& velocity, double angularVelocity)
-	{
-		this->Initialize(coords, elasticity, gravity, maxVelocity, velocity, angularVelocity);
-	}
-
-	void Initialize(const CoordStruct& coords, double elasticity, double gravity,
-			double maxVelocity, const Vector3D<float>& velocity, double angularVelocity)
-		{ JMP_THIS(0x4397E0); }
-
-	CoordStruct* Center(CoordStruct* pBuffer) const
-		{ JMP_THIS(0x4399A0); }
-
-	CoordStruct Center() const {
-		CoordStruct buffer;
-		this->Center(&buffer);
-		return buffer;
-	}
-
-	Matrix3D* GetDrawingMatrix(Matrix3D* pBuffer) const
-		{ JMP_THIS(0x4399E0); }
-
-	Matrix3D GetDrawingMatrix() const {
-		Matrix3D buffer;
-		this->GetDrawingMatrix(&buffer);
-		return buffer;
-	}
-
-	Status AI()
-		{ JMP_THIS(0x439B00); }
-
+public:
 	double Elasticity{ 0.0 }; // speed multiplier when bouncing off the ground
 	double Gravity{ 0.0 }; // subtracted from the Z coords every frame
 	double MaxVelocity{ 0.0 }; // 0.0 disables check
@@ -57,5 +22,21 @@ public:
 	Vector3D<float> Velocity; // speed components
 	Quaternion CurrentAngle; // quaternion for drawing
 	Quaternion AngularVelocity; // second quaternion as per-frame delta
+public:
+	BounceClass() = default;
+	BounceClass(const CoordStruct& coords, double elasticity, double gravity,
+		double maxVelocity, const Vector3D<float>& velocity, double angularVelocity)
+	{
+		this->Initialize(coords, elasticity, gravity, maxVelocity, velocity, angularVelocity);
+	}
+
+	void Initialize(const CoordStruct& coords, double elasticity, double gravity,
+			double maxVelocity, const Vector3D<float>& velocity, double angularVelocity) JMP_THIS(0x4397E0);
+
+	CoordStruct Center() const JMP_THIS(0x4399A0);
+	double Distance() JMP_THIS(0x439A10);
+	Matrix3D GetDrawingMatrix() const JMP_THIS(0x4399E0);
+
+	Status AI() JMP_THIS(0x439B00);
 };
 
