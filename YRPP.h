4d91b6712e28ce9f22793924b83c2d08d5d99303
Multfinite <multfinite@gmail.com>
2025-05-28 23:02:49 +0300

    YRPP include fixup

--- YRPP.h
+++ YRPP.h
@@ -7,11 +7,11 @@
 
 #pragma once
 
-#include <YRPPCore.h>
-#include <FileSystem.h>
-#include <StringTable.h>
-#include <Drawing.h>
-#include <PCX.h>
-#include <CommandClass.h>
-#include <GameClasses.h>
-#include <SwizzleManagerClass.h>
+#include "YRPPCore.h"
+#include "FileSystem.h"
+#include "StringTable.h"
+#include "Drawing.h"
+#include "PCX.h"
+#include "CommandClass.h"
+#include "GameClasses.h"
+#include "SwizzleManagerClass.h"
