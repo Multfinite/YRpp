0f412bb4260b2c1ea43e2d2d1dc7dd2cfa04be1e
Multfinite <multfinite@gmail.com>
2025-06-11 20:08:10 +0300

    BulletTypeClass revision #2

--- BulletTypeClass.h
+++ BulletTypeClass.h
@@ -13,12 +13,25 @@ class WarheadTypeClass;
 class NOVTABLE BulletTypeClass : public ObjectTypeClass
 {
 public:
+	using base_type = AbstractTypeClass;
+	struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+	{
+		constexpr vtables_t() noexcept : base_type::vtables_t()
+		{
+			this->IPersistStream = 0x7E4948;
+			this->IRTTITypeInfo = 0x7E492C;
+			this->INoticeSink = 0x7E4924;
+			this->INoticeSource = 0x7E491C;
+		}
+	};
+	static inline vtables_t vtables{};
+public:
 	static const AbstractType AbsID = AbstractType::BulletType;
 
-	//Array
+	static constexpr uintptr_t AbsVTable = 0x7E4948;
+
 	ABSTRACTTYPE_ARRAY(BulletTypeClass, 0xA83C80u);
-	static BulletTypeClass* __fastcall FindOrAllocate(const char* id)
-	{ JMP_STD(0x46C790); }
+	static BulletTypeClass* __fastcall FindOrAllocate(const char* id) JMP_STD(0x46C790);
 public:
 	bool Airburst;
 	bool Floater;
@@ -64,12 +77,10 @@ public:
 	byte AnimRate;
 	bool Flat;
 public:
+	virtual ~BulletTypeClass() JMP_THIS(0x46BE10);
 
 	//IPersist
 	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//Destructor
-	virtual ~BulletTypeClass() RX;
 
 	//AbstractClass
 	virtual RTTIType KindOf() const RT(AbstractType);
@@ -80,9 +91,7 @@ public:
 	virtual bool InstantiateAt(CellStruct* pMapCoords,HouseClass* pOwner) R0;
 	virtual ObjectClass* Instantiate(HouseClass* owner) R0;
 
-	bool Rotates() const {
-		return !this->NoRotate;
-	}
+	bool Rotates() const { return !this->NoRotate; }
 
 	void SetScaledSpawnDelay(int delay) {
 		// JMP_THIS(0x46C840);
@@ -98,13 +107,9 @@ public:
 		bool Bright)
 		{ JMP_STD(0x46B050); }
 
-	//Constructor
-	BulletTypeClass(const char* pID) noexcept
-		: BulletTypeClass(noinit_t())
-	{ JMP_THIS(0x46BBC0); }
-
 protected:
-	explicit __forceinline BulletTypeClass(noinit_t) noexcept
-		: ObjectTypeClass(noinit_t())
-	{ }
+	explicit __forceinline BulletTypeClass(fake_noinit_t) noexcept : ObjectTypeClass(fake_noinit_t{}) {}
+public:
+	BulletTypeClass(noinit_t) : BulletTypeClass(fake_noinit_t{}) JMP_THIS(0x46BDE0);
+	BulletTypeClass(const char* pId) : BulletTypeClass(fake_noinit_t{}) JMP_THIS(0x46BBC0);
 };
