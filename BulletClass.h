61c40a5de246281135c9d4c98901bce6eb6f7a32
Multfinite <multfinite@gmail.com>
2025-06-11 20:18:35 +0300

    BulletClass revision #1

--- BulletClass.h
+++ BulletClass.h
@@ -1,11 +1,8 @@
-/*
-	Projectiles
-*/
-
 #pragma once
 
-#include <ObjectClass.h>
-#include <BulletTypeClass.h>
+#include "Timer.h"
+#include "ObjectClass.h"
+#include "BulletTypeClass.h"
 
 class TechnoClass;
 class ObjectClass;
@@ -20,8 +17,11 @@ struct BulletData
 };
 
 // the velocities along the axes, or something like that
-using BulletVelocity = Vector3D<double>; // :3 -pd
+using BulletVelocity = Vector3D<double>;
 
+/*!
+* @brief Projectiles
+*/
 class NOVTABLE BulletClass : public ObjectClass
 {
 public:
@@ -31,7 +31,36 @@ public:
 	static constexpr constant_ptr<DynamicVectorClass<BulletClass*>, 0xA8ED40u> const Array{};
 
 	static constexpr reference<DynamicVectorClass<BulletClass*>, 0x89DE18u> const ScalableBullets{};
-
+public:
+	BulletTypeClass* Type;
+	TechnoClass* Owner;
+	bool IsInaccurate;
+	BulletData Data;
+	bool Bright;
+	DWORD unknown_E4;
+	BulletVelocity Velocity;
+	DWORD unknown_100;
+	bool unknown_104;
+	bool CourseLock;
+	int CourseLockCounter;
+	AbstractClass* Target;
+	int Speed;
+	int InheritedColor;
+	DWORD unknown_118;
+	DWORD unknown_11C;
+	double unknown_120;
+	WarheadTypeClass* WH;
+	byte AnimFrame;
+	byte AnimRateCounter;
+	WeaponTypeClass* WeaponType;
+	CoordStruct SourceCoords;
+	CoordStruct TargetCoords;
+	CellStruct LastMapCoords;
+	int DamageMultiplier;
+	AnimClass* NextAnim;
+	bool SpawnNextAnim;
+	int Range;
+public:
 	//IPersist
 	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
 
@@ -59,45 +88,31 @@ public:
 		int damage,
 		WarheadTypeClass* pWarhead,
 		int speed,
-		bool bright)
-		{ JMP_THIS(0x4664C0); }
+		bool bright) JMP_THIS(0x4664C0);
 
 	// calls Detonate with the appropriate coords
-	void Explode(bool destroy = false)
-		{ JMP_THIS(0x468D80); }
+	void Explode(bool destroy = false) JMP_THIS(0x468D80); 
 
 	// detonate the bullet at specific coords
-	void Detonate(const CoordStruct& coords)
-		{ JMP_THIS(0x4690B0); }
+	void Detonate(const CoordStruct& coords) JMP_THIS(0x4690B0); 
 
 	// spawns off the proper amount of shrapnel projectiles
-	void Shrapnel()
-		{ JMP_THIS(0x46A310); }
+	void Shrapnel() JMP_THIS(0x46A310);
 
-	static void ApplyRadiationToCell(CellStruct cell, int radius, int amount)
-		{ JMP_STD(0x46ADE0); }
+	static void ApplyRadiationToCell(CellStruct cell, int radius, int amount) JMP_STD(0x46ADE0);
 
 	// this bullet will miss and hit the ground instead.
 	// if the original target is in air, it will disappear.
-	void LoseTarget()
-		{ JMP_THIS(0x468430); }
-
-	bool IsHoming() const
-		{ return this->Type->ROT > 0; }
-
-	void SetWeaponType(WeaponTypeClass *weapon)
-		{ this->WeaponType = weapon; }
-
-	WeaponTypeClass * GetWeaponType() const
-		{ return this->WeaponType; }
+	void LoseTarget() JMP_THIS(0x468430);
+	bool IsHoming() const 	{ return this->Type->ROT > 0; }
+	void SetWeaponType(WeaponTypeClass *weapon) 	{ this->WeaponType = weapon; }
+	WeaponTypeClass * GetWeaponType() const	{ return this->WeaponType; }
 
 	// only called in UnitClass::Fire if Type->Scalable
-	void InitScalable()
-		{ JMP_THIS(0x46B280); }
+	void InitScalable() JMP_THIS(0x46B280);
 
 	// call only after the target, args, etc., have been set
-	void NukeMaker()
-		{ JMP_THIS(0x46B310); }
+	void NukeMaker() JMP_THIS(0x46B310);
 
 	// helpers
 	CoordStruct TargetCoord() const {
@@ -118,39 +133,4 @@ protected:
 	explicit __forceinline BulletClass(noinit_t) noexcept
 		: ObjectClass(noinit_t())
 	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
-
-public:
-
-	BulletTypeClass* Type;
-	TechnoClass* Owner;
-	bool unknown_B4;
-	BulletData Data;
-	bool Bright;
-	DWORD unknown_E4;
-	BulletVelocity Velocity;
-	DWORD unknown_100;
-	bool unknown_104;
-	bool CourseLock;
-	int CourseLockCounter;
-	AbstractClass* Target;
-	int Speed;
-	int InheritedColor;
-	DWORD unknown_118;
-	DWORD unknown_11C;
-	double unknown_120;
-	WarheadTypeClass* WH;
-	byte AnimFrame;
-	byte AnimRateCounter;
-	WeaponTypeClass* WeaponType;
-	CoordStruct SourceCoords;
-	CoordStruct TargetCoords;
-	CellStruct LastMapCoords;
-	int DamageMultiplier;
-	AnimClass* NextAnim;
-	bool SpawnNextAnim;
-	int Range;
 };
