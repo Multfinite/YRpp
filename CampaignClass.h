a1122b6eaa0211c99c7d6f1ba5619a7d73994da8
Multfinite <multfinite@gmail.com>
2025-06-22 22:09:01 +0300

    CampaignClass revision #1

--- CampaignClass.h
+++ CampaignClass.h
@@ -1,32 +1,63 @@
 #pragma once
 
-#include <GeneralDefinitions.h>
-#include <CCINIClass.h>
+#include "GeneralDefinitions.h"
+#include "CCINIClass.h"
+#include "AbstractTypeClass.h"
 
-class NOVTABLE CampaignClass : public AbstractTypeClass {
-
+/*!
+* @brief Campaign class handling campaign data and progression.
+*/
+class NOVTABLE CampaignClass : public AbstractTypeClass
+{
 public:
+	struct __declspec(align(sizeof(uintptr_t))) vtables_t : public AbstractTypeClass::vtables_t
+	{
+		constexpr vtables_t() noexcept : AbstractTypeClass::vtables_t()
+		{
+			// Addresses will be zeroed as per requirements
+			this->IPersistStream = 0x7E4A28;
+			this->IRTTITypeInfo = 0x7E4A0C;
+			this->INoticeSink = 0x7E4A04;
+			this->INoticeSource = 0x7E49FC;
+		}
+	};
+	static inline vtables_t vtables{};
+
 	static const AbstractType AbsID = AbstractType::Campaign;
-
-	CampaignClass(const char *name) noexcept
-		: CampaignClass(noinit_t())
-	{ JMP_THIS(0x46CB60); }
-
-protected:
-	explicit __forceinline CampaignClass(noinit_t) noexcept
-		: AbstractTypeClass(noinit_t())
-	{ }
-
+	static constexpr uintptr_t AbsVTable = 0x7E4A28;
+	static constexpr size_t ClassSize = 0x3A0;
 public:
-	static void __fastcall CreateFromINIList(CCINIClass *pINI)
-		{ JMP_STD(0x46CE10); }
-
-	static signed int __fastcall FindIndex(const char* name)
-		{ JMP_STD(0x46CC90); }
+	static constexpr reference<DynamicVectorClass<CampaignClass*>, 0x887300u> const Array{};
 
 public:
 	int idxCD;
 	char Scenario[512];
 	int FinalMovie;
 	wchar_t Description[128];
+public:
+	virtual ~CampaignClass() JMP_THIS(0x46CC10);
+
+	HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x46CF80);
+	
+	HRESULT Load(IStream* pStm) override JMP_THIS(0x46D000);
+	HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x46D050);
+	
+	RTTIType KindOf() const override JMP_THIS(0x46D080);
+	int SizeOf() const override JMP_THIS(0x46D070);
+	void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x46CFC0);
+	
+	bool LoadFromINI(CCINIClass* pINI) override JMP_THIS(0x46CCD0);
+public:
+	static void __fastcall CreateFromINIList(CCINIClass* pINI) JMP_STD(0x46CE10);
+	static signed int __fastcall FindIndex(const char* name) JMP_STD(0x46CC90);
+protected:
+	/*! @brief FAKE CTOR */
+	explicit __forceinline CampaignClass(fake_noinit_t) noexcept
+		: AbstractTypeClass(fake_noinit_t{}) { }
+
+public:
+	CampaignClass(noinit_t) : CampaignClass(fake_noinit_t{}) { vtables.init(this); }
+	CampaignClass(const char* name) : CampaignClass(fake_noinit_t{})
+		JMP_THIS(0x46CB60);
 };
+static_assert(sizeof(CampaignClass) == CampaignClass::ClassSize);
