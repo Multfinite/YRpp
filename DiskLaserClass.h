39ec057334c4121478a84b57718f1c0f12b5767b
Multfinite <multfinite@gmail.com>
2025-06-22 23:00:26 +0300

    DiskLaserClass  revision #1

--- DiskLaserClass.h
+++ DiskLaserClass.h
@@ -1,68 +1,70 @@
-/*
-	DiskLasers are the floating disks' purple lasers.
-*/
-
 #pragma once
 
-#include <AbstractClass.h>
+#include "AbstractClass.h"
 
 class LaserDrawClass;
 class TechnoClass;
 class WeaponTypeClass;
 
+/*!
+* @brief DiskLasers are the floating disks' purple lasers.
+*/
 class NOVTABLE DiskLaserClass : public AbstractClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::DiskLaser;
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public AbstractClass::vtables_t
+    {
+        constexpr vtables_t() noexcept : AbstractClass::vtables_t()
+        {
+            this->IPersistStream = 0x7E5FB8;
+            this->IRTTITypeInfo = 0x7E5F9C;
+            this->INoticeSink = 0x7E5F94;
+            this->INoticeSource = 0x7E5F8C;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	// static
-	static constexpr constant_ptr<DynamicVectorClass<DiskLaserClass*>, 0x8A0208u> const Array{};
-
-	static constexpr auto Radius = 240;
-	static constexpr reference<Point2D, 0x8A0180u, 16> DrawCoords {};
-
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm,BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~DiskLaserClass() RX;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	//non-virtual
-	void Fire(TechnoClass* pOwner, TechnoClass* pTarget, WeaponTypeClass* pWeapon, int nDamage)
-		{ JMP_THIS(0x4A71A0); }
-
-	void PointerGotInvalid(AbstractClass* pInvalid)
-		{ JMP_THIS(0x4A7900); }
-
-	//Constructor
-	DiskLaserClass() noexcept
-		: DiskLaserClass(noinit_t())
-	{ JMP_THIS(0x4A7A30); }
-
-protected:
-	explicit __forceinline DiskLaserClass(noinit_t) noexcept
-		: AbstractClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::DiskLaser;
+    static constexpr uintptr_t AbsVTable = 0x7E5FB8;
+    static constexpr size_t ClassSize = 0x40;
 
 public:
+    static constexpr constant_ptr<DynamicVectorClass<DiskLaserClass*>, 0x8A0208u> const Array{};
+    static constexpr auto Radius = 240;
+    static constexpr reference<Point2D, 0x8A0180u, 16> DrawCoords{};
 
-	TechnoClass* Owner;
-	TechnoClass* Target;
-	WeaponTypeClass* Weapon;
-	DWORD unknown_30;
-	DWORD unknown_34;
-	DWORD unknown_38;
-	int Damage;
+public:
+    TechnoClass* Owner;
+    TechnoClass* Target;
+    WeaponTypeClass* Weapon;
+    DWORD LogicState;
+    DWORD drawoffset34;
+    DWORD drawoffset38;
+    int Damage;
+
+public:
+    virtual ~DiskLaserClass() JMP_THIS(0x4A7B00);
+
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x4A7C30);
+    
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x4A7B90);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x4A7C10);
+   
+    RTTIType KindOf() const override JMP_THIS(0x4A7C80);
+    int SizeOf() const override JMP_THIS(0x4A7C70);
+    void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x4A7B80);
+    void AI() override JMP_THIS(0x4A7340);
+
+    void Fire(TechnoClass* pOwner, TechnoClass* pTarget, WeaponTypeClass* pWeapon, int nDamage) JMP_THIS(0x4A71A0);
+    void PointerGotInvalid(AbstractClass* pInvalid) JMP_THIS(0x4A7900);
+
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline DiskLaserClass(fake_noinit_t) noexcept
+        : AbstractClass(fake_noinit_t())
+    { }
+
+public:
+    DiskLaserClass() noexcept : DiskLaserClass(fake_noinit_t()) JMP_THIS(0x4A7A30);
 };
+static_assert(sizeof(DiskLaserClass) == DiskLaserClass::ClassSize);
