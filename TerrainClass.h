bd04e8274a7723297a91212be55774216c8be538
Multfinite <multfinite@gmail.com>
2025-06-26 18:58:07 +0300

    TerrainClass revision

--- TerrainClass.h
+++ TerrainClass.h
@@ -1,55 +1,83 @@
-/*
-	Trees
+/*!
+* @brief TerrainClass - handles trees and terrain objects
 */
-
 #pragma once
 
-#include <ObjectClass.h>
-#include <TerrainTypeClass.h>
-#include <StageClass.h>
+#include "ObjectClass.h"
+#include "TerrainTypeClass.h"
+#include "StageClass.h"
 
 class NOVTABLE TerrainClass : public ObjectClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::Terrain;
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public ObjectClass::vtables_t
+    {
+        constexpr vtables_t() noexcept : ObjectClass::vtables_t()
+        {
+            this->IPersistStream = 0x7F522C;
+            this->IRTTITypeInfo = 0x7F5224;
+            this->INoticeSink = 0x7F521C;
+            this->INoticeSource = 0x7F5200;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	//global array
-	static constexpr constant_ptr<DynamicVectorClass<TerrainClass*>, 0xA8E988u> const Array{};
-
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~TerrainClass() RX;
-
-	//identification
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	//Constructor, Destructor
-	TerrainClass(TerrainTypeClass* tt, CellStruct coords) noexcept
-		: TerrainClass(noinit_t())
-	{ JMP_THIS(0x71BB90); }
-
-protected:
-	explicit __forceinline TerrainClass(noinit_t) noexcept
-		: ObjectClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::Terrain;
+    static constexpr uintptr_t AbsVTable = 0x7F522C;
+    static constexpr size_t ClassSize = 0xE0;
 
 public:
+    static constexpr constant_ptr<DynamicVectorClass<TerrainClass*>, 0xA8E988u> const Array{};
 
-	StageClass Animation;
-	TerrainTypeClass* Type;
-	bool IsBurning; // this terrain object has been ignited
-	bool IsCrumbling; // finish the animation and uninit
-	RectangleStruct unknown_rect_D0;
+public:
+    StageClass Animation;
+    TerrainTypeClass* Type;
+    bool IsBurning;
+    bool IsCrumbling;
+    RectangleStruct unknown_rect_D0;
 
+public:
+    virtual ~TerrainClass() JMP_THIS(0x71B7B0);
+
+public:
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x71D310);
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x71CDA0);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x71CF30);
+    
+    void Detach(AbstractClass* target, bool all = true) override JMP_THIS(0x71CFD0);
+    RTTIType KindOf() const override JMP_THIS(0x71D300);
+    int SizeOf() const override JMP_THIS(0x71D2F0);
+    void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x71CF50);
+    void AI() override JMP_THIS(0x71C730);
+   
+    ObjectTypeClass* Type() const override JMP_THIS(0x71D150);
+    bool Limbo() override JMP_THIS(0x71C930);
+    bool Unlimbo(const Coordinate& coords, Dir256 dir) override JMP_THIS(0x71D000);
+    void MarkAllOccupationBits(const Coordinate& coord) override JMP_THIS(0x71C110);
+    void UnmarkAllOccupationBits(const Coordinate& coord) override JMP_THIS(0x71C070);
+    bool DrawIfVisible(RectangleStruct* pBounds, bool EvenIfCloaked, DWORD dwUnk3) const override JMP_THIS(0x71CC50);
+    void DrawIt(Point2D* location, RectangleStruct* bounds) const override JMP_THIS(0x71C1B0);
+    void DrawAgain(const Point2D& location, const RectangleStruct& bounds) const override JMP_THIS(0x71C360);
+    bool Mark(MarkType mark) override JMP_THIS(0x71BFB0);
+    RectangleStruct* GetRenderDimensions(RectangleStruct* pRect) override JMP_THIS(0x71D160);
+    DamageState ReceiveDamage(int* damage, int distance, WarheadTypeClass* warhead, ObjectClass* source, bool ignoreDefenses, bool preventPassengerEscape, HouseClass* attacker) override JMP_THIS(0x71B920);
+    bool Ignite() override JMP_THIS(0x71C5B0);
+    void Extinguish() override JMP_THIS(0x71C6B0);
+    Move CanEnterCell(CellClass* pDestCell, FacingType facing, int level, CellClass* pSourceCell, bool alt) const override JMP_THIS(0x71C4D0);
+/*
+    void Read_INI() JMP_THIS(0x71CA70);
+    void Start_To_Crumble() JMP_THIS(0x71C8E0);
+    void Write_INI() JMP_THIS(0x71CB90);
+*/
+
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline TerrainClass(fake_noinit_t) noexcept : ObjectClass(fake_noinit_t()) {}
+
+public:
+    TerrainClass(TerrainTypeClass* tt, CellStruct coords) noexcept : TerrainClass(fake_noinit_t())
+        JMP_THIS(0x71BB90);
+    TerrainClass() noexcept : TerrainClass(fake_noinit_t{})
+        JMP_THIS(0x71BDF0);
 };
+static_assert(sizeof(TerrainClass) == TerrainClass::ClassSize);
