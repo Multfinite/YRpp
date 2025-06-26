ce95c41c3fdbf2352635ef3a3cce5edb798671cd
Multfinite <multfinite@gmail.com>
2025-06-26 17:37:38 +0300

    WarheadTypeClass revision

--- WarheadTypeClass.h
+++ WarheadTypeClass.h
@@ -1,131 +1,137 @@
-/*
-	Warheads
-*/
-
 #pragma once
 
-#include <AbstractTypeClass.h>
+#include "AbstractTypeClass.h"
 
-//forward declarations
 class AnimTypeClass;
 class ParticleTypeClass;
 class VoxelAnimTypeClass;
 
 struct WarheadFlags {
-	bool ForceFire;
-	bool Retaliate;
-	bool PassiveAcquire;
+    bool ForceFire;
+    bool Retaliate;
+    bool PassiveAcquire;
 
-	WarheadFlags(bool FF = true, bool Retal = true, bool Acquire = true) : ForceFire(FF), Retaliate(Retal), PassiveAcquire(Acquire) {};
+    WarheadFlags(bool FF = true, bool Retal = true, bool Acquire = true)
+        : ForceFire(FF), Retaliate(Retal), PassiveAcquire(Acquire) {
+    };
 };
 
+/*!
+* @brief WarheadTypeClass - handles warhead type definitions and behaviors
+*/
 class NOVTABLE WarheadTypeClass : public AbstractTypeClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::WarheadType;
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public AbstractTypeClass::vtables_t
+    {
+        constexpr vtables_t() noexcept : AbstractTypeClass::vtables_t()
+        {
+            this->IPersistStream = 0x7F6B30;
+            this->IRTTITypeInfo = 0x7F6B14;
+            this->INoticeSink = 0x7F6B0C;
+            this->INoticeSource = 0x7F6B04;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	//Array
-	ABSTRACTTYPE_ARRAY(WarheadTypeClass, 0x8874C0u);
-
-	static WarheadTypeClass* __fastcall FindOrAllocate(const char* id)
-	{ JMP_STD(0x75E3B0); }
-
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~WarheadTypeClass() RX;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	//AbstractTypeClass
-
-	//Constructor
-	WarheadTypeClass(const char* pID)
-		: WarheadTypeClass(noinit_t())
-	{ JMP_THIS(0x75CEC0); }
-
-protected:
-	explicit __forceinline WarheadTypeClass(noinit_t)
-		: AbstractTypeClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::WarheadType;
+    static constexpr uintptr_t AbsVTable = 0x7F6B30;
+    static constexpr size_t ClassSize = 0x1D0;
 
 public:
+    ABSTRACTTYPE_ARRAY(WarheadTypeClass, 0x8874C0u);
 
-	double  Deform;
+public:
+    double Deform;
+    double Verses[0xB];
+    double ProneDamage;
+    int DeformTreshold;
+    TypeList<AnimTypeClass*> AnimList;
+    InfDeath InfDeath;
+    float CellSpread;
+    float CellInset;
+    float PercentAtMax;
+    bool CausesDelayKill;
+    int DelayKillFrames;
+    float DelayKillAtMax;
+    float CombatLightSize;
+    ParticleTypeClass* Particle;
+    bool Wall;
+    bool WallAbsoluteDestroyer;
+    bool PenetratesBunker;
+    bool Wood;
+    bool Tiberium;
+    bool unknown_bool_149;
+    bool Sparky;
+    bool Sonic;
+    bool Fire;
+    bool Conventional;
+    bool Rocker;
+    bool DirectRocker;
+    bool Bright;
+    bool CLDisableRed;
+    bool CLDisableGreen;
+    bool CLDisableBlue;
+    bool EMEffect;
+    bool MindControl;
+    bool Poison;
+    bool IvanBomb;
+    bool ElectricAssault;
+    bool Parasite;
+    bool Temporal;
+    bool IsLocomotor;
+    _GUID Locomotor;
+    bool Airstrike;
+    bool Psychedelic;
+    bool BombDisarm;
+    int Paralyzes;
+    bool Culling;
+    bool MakesDisguise;
+    bool NukeMaker;
+    bool Radiation;
+    bool PsychicDamage;
+    bool AffectsAllies;
+    bool Bullets;
+    bool Veinhole;
+    int ShakeXlo;
+    int ShakeXhi;
+    int ShakeYlo;
+    int ShakeYhi;
+    TypeList<VoxelAnimTypeClass*> DebrisTypes;
+    TypeList<int> DebrisMaximums;
+    int MaxDebris;
+    int MinDebris;
+    PROTECTED_PROPERTY(DWORD, unused_1CC);
 
-	double Verses [0xB];
+public:
+    virtual ~WarheadTypeClass() JMP_THIS(0x75D230);
 
-	double  ProneDamage;
-	int     DeformTreshold;
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x75E080);
+    
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x75E0C0);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x75E2C0);
+    
+    void Detach(AbstractClass* instance, bool all = true) override JMP_THIS(0x75E440);
+    RTTIType KindOf() const override JMP_THIS(0x75E500);
+    int SizeOf() const override JMP_THIS(0x75E4F0);
+    void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x75DEC0);
+    
+    bool LoadFromINI(CCINIClass* pINI) override JMP_THIS(0x75D3A0);
 
-	TypeList<AnimTypeClass*> AnimList;
+    static WarheadTypeClass* __fastcall FindOrAllocate(const char* id) JMP_STD(0x75E3B0);
+/*
+    WarheadTypeClass * Find_Or_Make_0() JMP_THIS(0x75E3B0);
+    int32_t From_Name() JMP_THIS(0x75E4A0);
+*/
 
-	InfDeath InfDeath;
-	float   CellSpread;
-	float   CellInset;
-	float   PercentAtMax;
-	bool    CausesDelayKill;
-	int     DelayKillFrames;
-	float   DelayKillAtMax;
-	float   CombatLightSize;
-	ParticleTypeClass* Particle;
-	bool    Wall;
-	bool    WallAbsoluteDestroyer;
-	bool    PenetratesBunker;
-	bool    Wood;
-	bool    Tiberium;
-	bool    unknown_bool_149;
-	bool    Sparky;
-	bool    Sonic;
-	bool    Fire;
-	bool    Conventional;
-	bool    Rocker;
-	bool    DirectRocker;
-	bool    Bright;
-	bool    CLDisableRed;
-	bool    CLDisableGreen;
-	bool    CLDisableBlue;
-	bool    EMEffect;
-	bool    MindControl;
-	bool    Poison;
-	bool    IvanBomb;
-	bool    ElectricAssault;
-	bool    Parasite;
-	bool    Temporal;
-	bool    IsLocomotor;
-	_GUID   Locomotor;
-	bool    Airstrike;
-	bool    Psychedelic;
-	bool    BombDisarm;
-	int     Paralyzes;
-	bool    Culling;
-	bool    MakesDisguise;
-	bool    NukeMaker;
-	bool    Radiation;
-	bool    PsychicDamage;
-	bool    AffectsAllies;
-	bool    Bullets;
-	bool    Veinhole;
-	int     ShakeXlo;
-	int     ShakeXhi;
-	int     ShakeYlo;
-	int     ShakeYhi;
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline WarheadTypeClass(fake_noinit_t) noexcept : AbstractTypeClass(fake_noinit_t()) {}
 
-	TypeList<VoxelAnimTypeClass*> DebrisTypes;
-	TypeList<int> DebrisMaximums;
-
-	int     MaxDebris;
-	int     MinDebris;
-	PROTECTED_PROPERTY(DWORD, unused_1CC); //???
+public:
+    WarheadTypeClass(const char* pID) noexcept : WarheadTypeClass(fake_noinit_t{})
+        JMP_THIS(0x75CEC0);
+    WarheadTypeClass(noinit_t) noexcept : WarheadTypeClass(fake_noinit_t{})
+        JMP_THIS(0x75D1B0);
 };
+static_assert(sizeof(WarheadTypeClass) == WarheadTypeClass::ClassSize);
