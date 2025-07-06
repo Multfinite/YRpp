d8e2123558b429c53791649eab6eceb3ffa340e2
Multfinite <multfinite@gmail.com>
2025-07-07 02:00:46 +0300

    TEventClass revision

--- TEventClass.h
+++ TEventClass.h
@@ -1,87 +1,82 @@
 #pragma once
 
-#include <AbstractClass.h>
+#include "AbstractClass.h"
 
-//forward declarations
 class SuperClass;
 class TechnoClass;
 class TeamTypeClass;
 
+/*!
+* @brief TEventClass - handles trigger events and their conditions
+*/
 class NOVTABLE TEventClass : public AbstractClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::Event;
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public AbstractClass::vtables_t
+    {
+        constexpr vtables_t() noexcept : AbstractClass::vtables_t()
+        {
+            this->IPersistStream = 0x7F5578;
+            this->IRTTITypeInfo = 0x7F555C;
+            this->INoticeSink = 0x7F5554;
+            this->INoticeSource = 0x7F554C; 
+        }
+    };
+    static inline vtables_t vtables{};
 
-	//Static
-	static constexpr constant_ptr<DynamicVectorClass<TEventClass*>, 0xB0F1A0u> const Array {};
-
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~TEventClass() RX;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	// you are responsible for doing INI::ReadString and strtok'ing it before calling
-	// this func only calls strtok again, doesn't know anything about buffers
-	void LoadFromINI()
-		JMP_THIS(0x71F4E0);
-
-	// you allocate the buffer for this, and save it to ini yourself after this returns
-	// this func only sprintf's the stuff it needs into buffer
-	void PrepareSaveToINI(char* buffer) const
-		JMP_THIS(0x71F390);
-
-	static TriggerAttachType __fastcall GetAttachType(int eventKind)
-		JMP_STD(0x71F680);
-
-	// used in TriggerClass::HaveEventsOccured , when trigger is repeating
-	// both need to be true to check this event as done
-	bool GetStateA() const
-		JMP_THIS(0x71F950);
-
-	bool GetStateB() const
-		JMP_THIS(0x71F9C0);
-
-	// main brain
-	bool HasOccured(
-		int eventKind,
-		HouseClass* pHouse,
-		ObjectClass* Object,
-		CDTimerClass* ActivationFrame,
-		bool* isRepeating
-	) const
-		JMP_THIS(0x71E940);
-
-	//Constructor
-	TEventClass() noexcept
-		: TEventClass(noinit_t())
-	{
-		JMP_THIS(0x71E6A0);
-	}
-
-protected:
-	explicit __forceinline TEventClass(noinit_t) noexcept
-		: AbstractClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::Event;
+    static constexpr uintptr_t AbsVTable = 0x7F5578;
+    static constexpr size_t ClassSize = 0x58;
 
 public:
-	int ArrayIndex;
-	TEventClass* NextEvent;
-	TriggerEvent EventKind;
-	TeamTypeClass* TeamType; // If this event needs to reference a team type, then this is the pointer to the team type object.
-	int Value;
-	char String[0x1C];
-	HouseClass* House;
+    static constexpr constant_ptr<DynamicVectorClass<TEventClass*>, 0xB0F1A0u> const Array{};
+
+public:
+    int ArrayIndex;
+    TEventClass* NextEvent;
+    TriggerEvent EventKind;
+    TeamTypeClass* TeamType;
+    int Value;
+    char String[0x1C];
+    HouseClass* House;
+
+public:
+    virtual ~TEventClass() noexcept { JMP_THIS(0x71E830); }
+
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x71F880);
+    
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x71F8C0);
+    
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x71F930);
+    void Detach(AbstractClass* target, bool all = true) override JMP_THIS(0x71F800);
+    RTTIType KindOf() const override JMP_THIS(0x71FA60);
+    int SizeOf() const override JMP_THIS(0x71FA50);
+    void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x71F820);
+    int ArrayIndex() const override JMP_THIS(0x71FA70);
+
+    // Event handling
+    void LoadFromINI() JMP_THIS(0x71F4E0);
+    void PrepareSaveToINI(char* buffer) const JMP_THIS(0x71F390);
+    static TriggerAttachType __fastcall GetAttachType(int eventKind) JMP_STD(0x71F680);
+    bool GetStateA() const JMP_THIS(0x71F950);
+    bool GetStateB() const JMP_THIS(0x71F9C0);
+    bool HasOccured(int eventKind, HouseClass* pHouse, ObjectClass* Object,
+        CDTimerClass* ActivationFrame, bool* isRepeating) const JMP_THIS(0x71E940);
+
+/*
+    int32_t Get_House() JMP_THIS(0x71FA30);
+    NeedType Needs() JMP_THIS(0x71F5B0);
+    void Set_House(HouseClass* a2) JMP_THIS(0x71FA40);
+    int32_t Static_CTOR() JMP_THIS(0x71E620);
+    void Static_DTOR() JMP_THIS(0x71E660);
+*/
+
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline TEventClass(fake_noinit_t) noexcept : AbstractClass(fake_noinit_t()) {}
+
+public:
+    TEventClass() noexcept : TEventClass(fake_noinit_t{}) JMP_THIS(0x71E6A0);
+    TEventClass(noinit_t) noexcept : TEventClass(fake_noinit_t{}) JMP_THIS(0x71E800);
 };
+static_assert(sizeof(TEventClass) == TEventClass::ClassSize);
