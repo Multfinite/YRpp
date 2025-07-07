0d6b21e3f65a82b8098fa78645ac88956cac067f
Multfinite <multfinite@gmail.com>
2025-07-08 01:48:09 +0300

    SmudgeTypeClass revision

--- SmudgeTypeClass.h
+++ SmudgeTypeClass.h
@@ -1,62 +1,77 @@
-/*
-	SmudgeTypes are initialized by INI files.
-*/
-
 #pragma once
 
-#include <ObjectTypeClass.h>
+#include "ObjectTypeClass.h"
 
+/*!
+* @brief SmudgeTypeClass - handles smudge type definitions
+*/
 class NOVTABLE SmudgeTypeClass : public ObjectTypeClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::SmudgeType;
+    using base_type = ObjectTypeClass;
 
-	//Array
-	ABSTRACTTYPE_ARRAY(SmudgeTypeClass, 0xA8EC18u);
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+    {
+        constexpr vtables_t() noexcept : base_type::vtables_t()
+        {
+            this->IPersistStream = 0x7F3528;
+            this->IRTTITypeInfo = 0x7F350C;
+            this->INoticeSink = 0x7F3504;
+            this->INoticeSource = 0x7F34FC;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm,BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~SmudgeTypeClass() RX;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	//ObjectTypeClass
-	virtual bool InstantiateAt(CellStruct* pMapCoords, HouseClass* pOwner) R0;
-	virtual ObjectClass* Instantiate(HouseClass* pOwner) R0;
-
-	//SmudgeTypeClass
-	virtual void DrawIt(const Point2D& Point, const RectangleStruct& Rect, int SmudgeData, int Height, const CellStruct& MapCoords) RX;
-
-	static void __fastcall LoadFromIniList(int idxTheatre)
-		{ JMP_STD(0x6B5490); }
-
-	//Constructor
-	SmudgeTypeClass(const char* pID) noexcept
-		: SmudgeTypeClass(noinit_t())
-	{ JMP_THIS(0x6B5260); }
-
-protected:
-	explicit __forceinline SmudgeTypeClass(noinit_t) noexcept
-		: ObjectTypeClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::SmudgeType;
+    static constexpr uintptr_t AbsVTable = 0x7F3528;
+    static constexpr size_t ClassSize = 0x2A4;
 
 public:
+    ABSTRACTTYPE_ARRAY(SmudgeTypeClass, 0xA8EC18u);
 
-	int ArrayIndex;
-	int Width;
-	int Height;
-	bool Crater;
-	bool Burn;
+public:
+    int ArrayIndex;
+    int Width;
+    int Height;
+    bool Crater;
+    bool Burn;
+
+public:
+    virtual ~SmudgeTypeClass() noexcept JMP_THIS(0x6B53A0);
+
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x6B58D0);
+    
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x6B5850);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x6B58B0);
+   
+    RTTIType KindOf() const override JMP_THIS(0x6B6130);
+    int SizeOf() const override JMP_THIS(0x6B6140);
+    void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x6B57F0);
+    int ArrayIndex() const override JMP_THIS(0x6B6150);
+   
+    bool LoadFromINI(CCINIClass* pINI) override JMP_THIS(0x6B56D0);
+    bool InstantiateAt(CellStruct& position, HouseClass* pOwner) override JMP_THIS(0x6B5550);
+    ObjectClass* Instantiate(HouseClass* pOwner) override JMP_THIS(0x6B55C0);
+    
+    virtual void DrawIt(const Point2D& Point, const RectangleStruct& Rect, int SmudgeData, int Height, const CellStruct& MapCoords) JMP_THIS(0x6B55F0);
+
+    static void __fastcall LoadFromIniList(int idxTheatre) JMP_STD(0x6B5490);
+
+/*
+    int8_t Can_Place(Cell* cell, bool ignore_buildings) JMP_THIS(0x6B5F80);
+    SmudgeTypeClass* Find_Or_Make() JMP_THIS(0x6B5910);
+    int32_t From_Name() JMP_THIS(0x6B5440);
+    int32_t Init() JMP_THIS(0x6B5490);
+    void Place(Cell* a2) JMP_THIS(0x6B6080);
+*/
+
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline SmudgeTypeClass(fake_noinit_t) noexcept : base_type(fake_noinit_t()) {}
+
+public:
+    SmudgeTypeClass(const char* pID) noexcept : SmudgeTypeClass(fake_noinit_t()) JMP_THIS(0x6B5260);
+    SmudgeTypeClass(noinit_t) noexcept : SmudgeTypeClass(fake_noinit_t{}) JMP_THIS(0x6B5370);
 };
+
+static_assert(sizeof(SmudgeTypeClass) == SmudgeTypeClass::ClassSize);
