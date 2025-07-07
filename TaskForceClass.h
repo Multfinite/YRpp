73667c6cbcfbe121c961b101cbd1c53bed289cd7
Multfinite <multfinite@gmail.com>
2025-07-07 19:54:25 +0300

    TaskForceClass revision

--- TaskForceClass.h
+++ TaskForceClass.h
@@ -1,60 +1,69 @@
-/*
-	TaskForces as in the AI inis
-*/
-
 #pragma once
 
 #include "AbstractTypeClass.h"
 
-//forward declarations
 class TechnoTypeClass;
 
 struct TaskForceEntryStruct
 {
-	int Amount;
-	TechnoTypeClass* Type;
+    int Amount;
+    TechnoTypeClass* Type;
 };
 
+/*!
+* @brief TaskForceClass - handles AI task force definitions
+*/
 class NOVTABLE TaskForceClass : public AbstractTypeClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::TaskForce;
+    using base_type = AbstractTypeClass;
 
-	//Array
-	ABSTRACTTYPE_ARRAY(TaskForceClass, 0xA8E8D0u);
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+    {
+        constexpr vtables_t() noexcept : base_type::vtables_t()
+        {
+            this->IPersistStream = 0x7F4680;
+            this->IRTTITypeInfo = 0x7F4664;
+            this->INoticeSink = 0x7F465C;
+            this->INoticeSource = 0x7F4654;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~TaskForceClass() RX;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	//Constructor
-	TaskForceClass(const char* pID) noexcept
-		: TaskForceClass(noinit_t())
-	{ JMP_THIS(0x6E7E80); }
-
-protected:
-	explicit __forceinline TaskForceClass(noinit_t) noexcept
-		: AbstractTypeClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::TaskForce;
+    static constexpr uintptr_t AbsVTable = 0x7F4680;
+    static constexpr size_t ClassSize = 0xD4;
 
 public:
+    ABSTRACTTYPE_ARRAY(TaskForceClass, 0xA8E8D0u);
 
-	int     Group;
-	int     CountEntries;
-	bool    IsGlobal;
-	TaskForceEntryStruct Entries [0x6];
+public:
+    int Group;
+    int CountEntries;
+    bool IsGlobal;
+    TaskForceEntryStruct Entries[0x6];
+
+public:
+    virtual ~TaskForceClass() noexcept JMP_THIS(0x6E7F80);
+
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x6E8710);
+    
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x6E86A0);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x6E8680);
+    
+    RTTIType KindOf() const override JMP_THIS(0x6E87D0);
+    int SizeOf() const override JMP_THIS(0x6E87E0);
+    void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x6E8750);
+   
+    bool LoadFromINI(CCINIClass* pINI) override JMP_THIS(0x6E8420);
+    bool SaveToINI(CCINIClass* pINI) override JMP_THIS(0x6E8510);
+
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline TaskForceClass(fake_noinit_t) noexcept : base_type(fake_noinit_t{}) {}
+
+public:
+    TaskForceClass(const char* pID) noexcept : TaskForceClass(fake_noinit_t{}) JMP_THIS(0x6E7E80);
+    TaskForceClass(noinit_t) noexcept : TaskForceClass(fake_noinit_t{}) { vtables.init(this); }
 };
+static_assert(sizeof(TaskForceClass) == TaskForceClass::ClassSize);
