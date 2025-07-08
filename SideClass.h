6c2adab5f1ce20ea3fce484e21115e2df738ce17
Multfinite <multfinite@gmail.com>
2025-07-08 16:37:32 +0300

    SideClass revision

--- SideClass.h
+++ SideClass.h
@@ -1,7 +1,3 @@
-/*
-	Sides
-*/
-
 #pragma once
 
 #include "AbstractTypeClass.h"
@@ -9,41 +5,50 @@
 class NOVTABLE SideClass : public AbstractTypeClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::Side;
+    using base_type = AbstractTypeClass;
 
-	//Array
-	ABSTRACTTYPE_ARRAY(SideClass, 0x8B4120u);
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+    {
+        constexpr vtables_t() noexcept : base_type::vtables_t()
+        {
+            this->IPersistStream = 0x7F2EC0;
+            this->IRTTITypeInfo = 0x7F2EA4;
+            this->INoticeSink = 0x7F2E9C;
+            this->INoticeSource = 0x7F2E94;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm,BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~SideClass() RX;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	//Constructor
-	SideClass(const char* pID) noexcept
-		: SideClass(noinit_t())
-	{ JMP_THIS(0x6A4550); }
-
-protected:
-	explicit __forceinline SideClass(noinit_t) noexcept
-		: AbstractTypeClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::Side;
+    static constexpr uintptr_t AbsVTable = 0x7F2EC0;
+    static constexpr size_t ClassSize = 0xB4;
 
 public:
+    ABSTRACTTYPE_ARRAY(SideClass, 0x8B4120u);
 
-	TypeList<int> HouseTypes;	//indices!
+public:
+    /*! @brief Indexes */
+    TypeList<int> HouseTypes;
 
+public:
+    virtual ~SideClass() noexcept JMP_THIS(0x6A4610);
+
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x6A4740);
+    
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x6A4780);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x6A48A0);
+    
+    RTTIType KindOf() const override JMP_THIS(0x6A4920);
+    int SizeOf() const override JMP_THIS(0x6A4910);
+    void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x6A4710);
+
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline SideClass(fake_noinit_t) noexcept : base_type(fake_noinit_t()) {}
+
+public:
+    SideClass(const char* pID) noexcept : SideClass(fake_noinit_t()) JMP_THIS(0x6A4550);
+    SideClass(noinit_t) noexcept : SideClass(fake_noinit_t{}) { vtables.init(this); }
 };
+
+static_assert(sizeof(SideClass) == SideClass::ClassSize);
