b64f1c36637b3df7e7bcc5209a9171b4a95a9eb9
Multfinite <multfinite@gmail.com>
2025-07-09 14:54:00 +0300

    BulletClass revision

--- BulletClass.h
+++ BulletClass.h
@@ -25,112 +25,106 @@ using BulletVelocity = Vector3D<double>;
 class NOVTABLE BulletClass : public ObjectClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::Bullet;
+    using base_type = ObjectClass;
 
-	//Array
-	static constexpr constant_ptr<DynamicVectorClass<BulletClass*>, 0xA8ED40u> const Array{};
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+    {
+        constexpr vtables_t() noexcept : base_type::vtables_t()
+        {
+            this->IPersistStream = 0x7E46E4;
+            this->IRTTITypeInfo = 0x7E46C8;
+            this->INoticeSink = 0x7E46C0;
+            this->INoticeSource = 0x7E46B8;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	static constexpr reference<DynamicVectorClass<BulletClass*>, 0x89DE18u> const ScalableBullets{};
+    static const AbstractType AbsID = AbstractType::Bullet;
+    static constexpr uintptr_t AbsVTable = 0x7E46E4;
+    static constexpr size_t ClassSize = 0x160;
+
 public:
-	BulletTypeClass* Type;
-	TechnoClass* Owner;
-	bool IsInaccurate;
-	BulletData Data;
-	bool Bright;
-	DWORD unknown_E4;
-	BulletVelocity Velocity;
-	DWORD unknown_100;
-	bool unknown_104;
-	bool CourseLock;
-	int CourseLockCounter;
-	AbstractClass* Target;
-	int Speed;
-	int InheritedColor;
-	DWORD unknown_118;
-	DWORD unknown_11C;
-	double unknown_120;
-	WarheadTypeClass* WH;
-	byte AnimFrame;
-	byte AnimRateCounter;
-	WeaponTypeClass* WeaponType;
-	CoordStruct SourceCoords;
-	CoordStruct TargetCoords;
-	CellStruct LastMapCoords;
-	int DamageMultiplier;
-	AnimClass* NextAnim;
-	bool SpawnNextAnim;
-	int Range;
+    static constexpr constant_ptr<DynamicVectorClass<BulletClass*>, 0xA8ED40u> const Array{};
+    static constexpr reference<DynamicVectorClass<BulletClass*>, 0x89DE18u> const ScalableBullets{};
+
 public:
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
+    BulletTypeClass* Type;
+    TechnoClass* Owner;
+    bool IsInaccurate;
+    BulletData Data;
+    bool Bright;
+    DWORD unknown_E4;
+    BulletVelocity Velocity;
+    DWORD unknown_100;
+    bool unknown_104;
+    bool CourseLock;
+    int CourseLockCounter;
+    AbstractClass* Target;
+    int Speed;
+    int InheritedColor;
+    DWORD unknown_118;
+    DWORD unknown_11C;
+    double unknown_120;
+    WarheadTypeClass* WH;
+    byte AnimFrame;
+    byte AnimRateCounter;
+    WeaponTypeClass* WeaponType;
+    CoordStruct SourceCoords;
+    CoordStruct TargetCoords;
+    CellStruct LastMapCoords;
+    int DamageMultiplier;
+    AnimClass* NextAnim;
+    bool SpawnNextAnim;
+    int Range;
 
-	//IPersistStream
-	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) R0;
+public:
+    virtual ~BulletClass() noexcept JMP_THIS(0x466560);
 
-	//Destructor
-	virtual ~BulletClass() RX;
+    ULONG AddRef() override JMP_THIS(0x46AFD0);
+    ULONG Release() override JMP_THIS(0x46AFF0);
+    
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x46B560);
+    
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x46AE70);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x46AFB0);
+    
+    void Detach(AbstractClass* target, bool all = true) override JMP_THIS(0x4684E0);
+    RTTIType KindOf() const override JMP_THIS(0x46B550);
+    int SizeOf() const override JMP_THIS(0x46B540);
+    void AI() override JMP_THIS(0x4666E0);
+    
+    Layer InWhichLayer() const override JMP_THIS(0x468B90);
+    ObjectTypeClass* Type() const override JMP_THIS(0x46B5B0);
+    CellStruct* OccupiedCells(bool placement) const override JMP_THIS(0x466660);
+    void DrawIt(Point2D* location, RectangleStruct* bounds) const override JMP_THIS(0x468090);
+    bool Mark(MarkType mark) override JMP_THIS(0x4666C0);
 
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
+    virtual BYTE GetAnimFrame() const JMP_THIS(0x468000);
+    virtual void SetTarget(AbstractClass* pTarget) JMP_THIS(0x46B5A0);
+    virtual bool MoveTo(const CoordStruct& where, const BulletVelocity& velocity) JMP_THIS(0x468670);
 
-	//BulletClass
-	virtual BYTE GetAnimFrame() const R0;
-	virtual void SetTarget(AbstractClass* pTarget) RX;
-	virtual bool MoveTo(const CoordStruct& where, const BulletVelocity& velocity) R0;
+    void Construct(BulletTypeClass* pType, AbstractClass* pTarget, TechnoClass* pOwner,
+        int damage, WarheadTypeClass* pWarhead, int speed, bool bright) JMP_THIS(0x4664C0);
+    void Explode(bool destroy = false) JMP_THIS(0x468D80);
+    void Detonate(const CoordStruct& coords) JMP_THIS(0x4690B0);
+    void Shrapnel() JMP_THIS(0x46A310);
+    static void ApplyRadiationToCell(CellStruct cell, int radius, int amount) JMP_STD(0x46ADE0);
+    void LoseTarget() JMP_THIS(0x468430);
+    void InitScalable() JMP_THIS(0x46B280);
+    void NukeMaker() JMP_THIS(0x46B310);
 
-	// non-virtual
-	// after CoCreateInstance creates a bullet, this configures it
-	void Construct(
-		BulletTypeClass* pType,
-		AbstractClass* pTarget,
-		TechnoClass* pOwner,
-		int damage,
-		WarheadTypeClass* pWarhead,
-		int speed,
-		bool bright) JMP_THIS(0x4664C0);
-
-	// calls Detonate with the appropriate coords
-	void Explode(bool destroy = false) JMP_THIS(0x468D80); 
-
-	// detonate the bullet at specific coords
-	void Detonate(const CoordStruct& coords) JMP_THIS(0x4690B0); 
-
-	// spawns off the proper amount of shrapnel projectiles
-	void Shrapnel() JMP_THIS(0x46A310);
-
-	static void ApplyRadiationToCell(CellStruct cell, int radius, int amount) JMP_STD(0x46ADE0);
-
-	// this bullet will miss and hit the ground instead.
-	// if the original target is in air, it will disappear.
-	void LoseTarget() JMP_THIS(0x468430);
-	bool IsHoming() const 	{ return this->Type->ROT > 0; }
-	void SetWeaponType(WeaponTypeClass *weapon) 	{ this->WeaponType = weapon; }
-	WeaponTypeClass * GetWeaponType() const	{ return this->WeaponType; }
-
-	// only called in UnitClass::Fire if Type->Scalable
-	void InitScalable() JMP_THIS(0x46B280);
-
-	// call only after the target, args, etc., have been set
-	void NukeMaker() JMP_THIS(0x46B310);
-
-	// helpers
-	CoordStruct TargetCoord() const {
-		if(this->Target) {
-			return this->Target->Center();
-		} else {
-			return this->Center();
-		}
-	}
-
-	//Constructor
-protected:
-	BulletClass() noexcept
-		: BulletClass(noinit_t())
-	{ JMP_THIS(0x466380); }
+    bool IsHoming() const { return this->Type->ROT > 0; }
+    void SetWeaponType(WeaponTypeClass* weapon) { this->WeaponType = weapon; }
+    WeaponTypeClass* GetWeaponType() const { return this->WeaponType; }
+    CoordStruct TargetCoord() const { return this->Target ? this->Target->Center() : this->Center(); }
 
 protected:
-	explicit __forceinline BulletClass(noinit_t) noexcept
-		: ObjectClass(noinit_t())
-	{ }
+    /*! @brief FAKE CTOR */
+    explicit __forceinline BulletClass(fake_noinit_t) noexcept : base_type(fake_noinit_t()) {}
+
+public:
+    BulletClass() noexcept : BulletClass(fake_noinit_t()) JMP_THIS(0x466380);
+    BulletClass(noinit_t) noexcept : BulletClass(fake_noinit_t{}) { vtables.init(this); }
 };
+
+static_assert(sizeof(BulletClass) == BulletClass::ClassSize);
