5f79c4e17fd6d57f394c2d8618dbc8d198eb2342
Multfinite <multfinite@gmail.com>
2025-07-09 16:18:23 +0300

    IsometricTileClass revision

--- IsometricTileClass.h
+++ IsometricTileClass.h
@@ -1,51 +1,59 @@
 #pragma once
 
-#include <ObjectClass.h>
+#include "ObjectClass.h"
 
-//forward declarations
 class IsometricTileTypeClass;
 
 class NOVTABLE IsometricTileClass : public ObjectClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::Isotile;
+    using base_type = ObjectClass;
 
-	//Array
-	static constexpr constant_ptr<DynamicVectorClass<IsometricTileClass*>, 0x87F750u> Array{};
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+    {
+        constexpr vtables_t() noexcept : base_type::vtables_t()
+        {
+            this->IPersistStream = 0x7EC258;
+            this->IRTTITypeInfo = 0x7EC23C;
+            this->INoticeSink = 0x7EC234;
+            this->INoticeSource = 0x7EC22C;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) override R0;
-
-	virtual HRESULT __stdcall Load(IStream* pStm) override R0;
-	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) override R0;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const override RT(AbstractType);
-	virtual int SizeOf() const override R0;
-
-	//ObjectClass
-	virtual ObjectTypeClass* Type() const override R0;
-	virtual bool Limbo() override R0;
-	virtual bool Unlimbo(const CoordStruct& Crd, Dir256 dFaceDir) override R0;
-	virtual void Draw(Point2D* pLocation, RectangleStruct* pBounds) const override RX;
-
-	//Destructor
-	virtual ~IsometricTileClass() RX;
-
-	//Constructor
-	IsometricTileClass(int idxType, CellStruct const& location) noexcept
-		: IsometricTileClass(noinit_t())
-	{ JMP_THIS(0x543780); }
-
-protected:
-	explicit __forceinline IsometricTileClass(noinit_t) noexcept
-		: ObjectClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::Isotile;
+    static constexpr uintptr_t AbsVTable = 0x7EC258;
+    static constexpr size_t ClassSize = 0xB0;
 
 public:
-	IsometricTileTypeClass* Type;
+    static constexpr constant_ptr<DynamicVectorClass<IsometricTileClass*>, 0x87F750u> const Array{};
+
+public:
+    IsometricTileTypeClass* Type;
+
+public:
+    virtual ~IsometricTileClass() noexcept JMP_THIS(0x543880);
+
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x543AB0);
+    
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x543990);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x5439F0);
+    
+    RTTIType KindOf() const override JMP_THIS(0x543AA0);
+    int SizeOf() const override JMP_THIS(0x543A90);
+    
+    ObjectTypeClass* Type() const override JMP_THIS(0x543AF0);
+    bool Limbo() override JMP_THIS(0x543A40);
+    bool Unlimbo(const Coordinate& position, Dir256 dir) override JMP_THIS(0x543A10);
+    void DrawIt(Point2D* location, RectangleStruct* bounds) const override JMP_THIS(0x543B00);
+    bool Mark(MarkType mark) override JMP_THIS(0x543330);
+
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline IsometricTileClass(fake_noinit_t) noexcept : base_type(fake_noinit_t()) {}
+
+public:
+    IsometricTileClass(int idxType, CellStruct const& location) noexcept : IsometricTileClass(fake_noinit_t()) JMP_THIS(0x543780);
+    IsometricTileClass(noinit_t) noexcept : IsometricTileClass(fake_noinit_t()) { vtables.init(this); }
 };
+static_assert(sizeof(IsometricTileClass) == IsometricTileClass::ClassSize);
