2000e6c4de6843422e5d112b3cc1df1b0401c32f
Multfinite <multfinite@gmail.com>
2025-07-12 13:01:55 +0300

    EMPulseClass revision

--- EMPulseClass.h
+++ EMPulseClass.h
@@ -1,51 +1,61 @@
-/*
-	EMP - no, you're NOT seeing things :P
-*/
-
 #pragma once
 
-#include <AbstractClass.h>
+#include "AbstractClass.h"
 
 class NOVTABLE EMPulseClass : public AbstractClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::EMPulse;
+    using base_type = AbstractClass;
 
-	//Static
-	static constexpr constant_ptr<DynamicVectorClass<EMPulseClass*>, 0x8A3870u> const Array{};
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+    {
+        constexpr vtables_t() noexcept : base_type::vtables_t()
+        {            
+            this->IPersistStream = 0x7E87A8;
+            this->IRTTITypeInfo = 0x7E878C;
+            this->INoticeSink = 0x7E8784;
+            this->INoticeSource = 0x7E877C;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm,BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~EMPulseClass() RX;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	//Constructor
-	EMPulseClass(CellStruct dwCrd, int nSpread, int nDuration,
-		TechnoClass* pGenerator) noexcept : EMPulseClass(noinit_t())
-	{ JMP_THIS(0x4C52B0); }
-
-protected:
-	explicit __forceinline EMPulseClass(noinit_t) noexcept
-		: AbstractClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::EMPulse;
+    static constexpr uintptr_t AbsVTable = 0x7E87A8;
+    static constexpr size_t ClassSize = 0x34;
 
 public:
+    static constexpr constant_ptr<DynamicVectorClass<EMPulseClass*>, 0x8A3870u> const Array{};
 
-	CellStruct BaseCoords;
-	int Spread;
-	int CreationTime;	//frame in which this EMP got created
-	int Duration;
+public:
+    CellStruct BaseCoords;
+    int Spread;
+    int CreationTime;
+    int Duration;
+
+public:
+    virtual ~EMPulseClass() JMP_THIS(0x4C53E0);
+
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x4C59F0);
+    
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x4C5A30);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x4C5A80);
+    
+    RTTIType KindOf() const override JMP_THIS(0x4C5AB0);
+    int SizeOf() const override JMP_THIS(0x4C5AA0);
+    void ComputeCRC(CRCEngine& crc) const override JMP_THIS(0x4C59A0);
+
+/*
+    int32_t AI() JMP_THIS(0x4C54A0);
+    void Create(int32_t arg0) JMP_THIS(0x4C54E0);
+    int32_t Init_Clear() JMP_THIS(0x4C5470);
+*/
+
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline EMPulseClass(fake_noinit_t) noexcept : base_type(fake_noinit_t()) {}
+public:
+    EMPulseClass(CellStruct dwCrd, int nSpread, int nDuration, TechnoClass* pGenerator) noexcept : EMPulseClass(fake_noinit_t()) JMP_THIS(0x4C52B0);
+    EMPulseClass() noexcept : EMPulseClass(fake_noinit_t{}) JMP_THIS(0x4C5370);
+    EMPulseClass(noinit_t) noexcept : EMPulseClass(fake_noinit_t{}) JMP_THIS(0x4C53B0);
 };
+static_assert(sizeof(EMPulseClass) == EMPulseClass::ClassSize);
