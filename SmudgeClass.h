7ad47ad61006e7a6f523975128370bb53336e46a
Multfinite <multfinite@gmail.com>
2025-07-12 12:44:49 +0300

    SmudgeClass revision

--- SmudgeClass.h
+++ SmudgeClass.h
@@ -1,50 +1,57 @@
-/*
-	Smudges
-*/
-
 #pragma once
 
-#include <ObjectClass.h>
-#include <SmudgeTypeClass.h>
+#include "ObjectClass.h"
+#include "SmudgeTypeClass.h"
 
 class NOVTABLE SmudgeClass : public ObjectClass
 {
 public:
-	static const AbstractType AbsID = AbstractType::Smudge;
+    using base_type = ObjectClass;
 
-	//Static
-	static constexpr constant_ptr<DynamicVectorClass<SmudgeClass*>, 0xA8B1E0u> const Array{};
+    struct __declspec(align(sizeof(uintptr_t))) vtables_t : public base_type::vtables_t
+    {
+        constexpr vtables_t() noexcept : base_type::vtables_t()
+        {
+            this->IPersistStream = 0x7F32FC;
+            this->IRTTITypeInfo = 0x7F32F4;
+            this->INoticeSink = 0x7F32D8;
+            this->INoticeSource = 0x7F32D0;
+        }
+    };
+    static inline vtables_t vtables{};
 
-	//IPersist
-	virtual HRESULT __stdcall GetClassID(CLSID* pClassID) R0;
-
-	//IPersistStream
-	virtual HRESULT __stdcall Load(IStream* pStm) R0;
-	virtual HRESULT __stdcall Save(IStream* pStm, BOOL fClearDirty) R0;
-
-	//Destructor
-	virtual ~SmudgeClass() RX;
-
-	//AbstractClass
-	virtual RTTIType KindOf() const RT(AbstractType);
-	virtual int SizeOf() const R0;
-
-	//Constructor
-	SmudgeClass(SmudgeTypeClass* pType) noexcept
-		: SmudgeClass(noinit_t())
-	{ JMP_THIS(0x6B4A50); }
-
-protected:
-	explicit __forceinline SmudgeClass(noinit_t) noexcept
-		: ObjectClass(noinit_t())
-	{ }
-
-	//===========================================================================
-	//===== Properties ==========================================================
-	//===========================================================================
+    static const AbstractType AbsID = AbstractType::Smudge;
+    static constexpr uintptr_t AbsVTable = 0x7F32FC;
+    static constexpr size_t ClassSize = 0xB0;
 
 public:
+    static constexpr constant_ptr<DynamicVectorClass<SmudgeClass*>, 0xA8B1E0u> const Array{};
 
-	SmudgeTypeClass* Type;
+public:
+    SmudgeTypeClass* Type;
 
+public:
+    virtual ~SmudgeClass() noexcept JMP_THIS(0x6B4B40);
+   
+    HRESULT GetClassID(CLSID* pClassID) override JMP_THIS(0x6B4F50);
+   
+    HRESULT Load(IStream* pStm) override JMP_THIS(0x6B4EA0);
+    HRESULT Save(IStream* pStm, BOOL fClearDirty) override JMP_THIS(0x6B4F00);
+    
+    RTTIType KindOf() const override JMP_THIS(0x6B4F40);
+    int SizeOf() const override JMP_THIS(0x6B4F30);
+    ObjectTypeClass* Type() const override JMP_THIS(0x6B4F20);
+    
+    void DrawIt(Point2D* location, RectangleStruct* bounds) const override JMP_THIS(0x6B4F90);
+    bool Mark(MarkType value) override JMP_THIS(0x6B4BE0);
+
+protected:
+    /*! @brief FAKE CTOR */
+    explicit __forceinline SmudgeClass(fake_noinit_t) noexcept : base_type(fake_noinit_t()) {}
+
+public:
+    SmudgeClass(SmudgeTypeClass const* pType, Coordinate const& pos, int houseId) noexcept : SmudgeClass(fake_noinit_t()) JMP_THIS(0x6B4A50);
+    SmudgeClass(noinit_t) noexcept : SmudgeClass(fake_noinit_t{}) { vtables.init(this); }
 };
+
+static_assert(sizeof(SmudgeClass) == SmudgeClass::ClassSize);
