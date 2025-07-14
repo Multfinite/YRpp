edcd61e371afdcb0191871aaf146c5bf32973e4d
Multfinite <multfinite@gmail.com>
2025-07-14 20:58:14 +0300

    woops remove debug

--- MissionClass.h
+++ MissionClass.h
@@ -335,6 +335,5 @@ protected:
 	explicit __forceinline MissionClass(fake_noinit_t) noexcept : ObjectClass(fake_noinit_t{}), UpdateTimer(noinit_t{}) {}
 	MissionClass(noinit_t) noexcept : MissionClass(fake_noinit_t{}) { vtables.init(this); };
 	MissionClass() : MissionClass(fake_noinit_t{}) JMP_THIS(0x5B2DA0);
-	void g();
 };
 static_assert(sizeof(MissionClass) == MissionClass::ClassSize);
