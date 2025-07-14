6570c6d554cd6e65a4d55436443e674c4043b07c
Multfinite <multfinite@gmail.com>
2025-07-14 18:53:50 +0300

    TechnoType() -> GetTechnoType() CCINIClass

--- CCINIClass.h
+++ CCINIClass.h
@@ -278,7 +278,7 @@ public:
 		{ JMP_THIS(0x477640); }
 
 
-	TechnoTypeClass* TechnoType(const char* pSection, const char* pKey)
+	TechnoTypeClass* GetTechnoType(const char* pSection, const char* pKey)
 		{ JMP_THIS(0x476EB0); }
 
 	// safer and more convenient overload for string reading
